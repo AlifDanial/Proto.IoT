dc2cab7f63b675bcc80cdb513665cff03ec90ee2
AlifDanial <aldmnsri@gmail.com>
2019-11-04 18:23:56 +0800

    UI adjustments and functionalities

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+   private void Awake()
+{
+    //  image = GetComponent<UnityEngine.UI.Image>();
+
+     HideImage();
+}
+
+public void HideImage()
+{
+    //  image.enabled = false ;
+
+     // ===== OR =====
+
+     gameObject.SetActive(false);
+}
+
+
+
+
+
+}
