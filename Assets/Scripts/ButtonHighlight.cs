f255ee8d02ba0a862a8dbcdfd00d77c34cf8dfa2
Muhammad Alif Danial <aldmnsri@gmail.com>
2020-01-04 12:42:47 +0800

    Forgotten Commit

---
+++ Assets/Scripts/ButtonHighlight.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHighlight : MonoBehaviour { 
+
+    public Button btn1;
+    public Button btn2;
+    public Button btn3;
+    public Button btn4;
+
+    public Button btn5;
+    public Button btn6;
+    public Button btn7;
+    public Button btn8;
+    public Button btn9;
+    public Button btn10;
+
+    public GameObject RFID;   
+    public GameObject MOTION;
+    public GameObject TOUCH;
+    public GameObject SOUND;
+
+    public GameObject AUDIO;
+    public GameObject LED;
+    public GameObject TEXT;
+    public GameObject MOTOR;
+    public GameObject VIDEO;
+    public GameObject GRAPHICS;
+
+    void Start()
+    {
+        InitializeColors(btn1);
+        InitializeColors(btn2);
+        InitializeColors(btn3);
+        InitializeColors(btn4);
+
+        InitializeColors2(btn5);
+        InitializeColors2(btn6);
+        InitializeColors2(btn7);
+        InitializeColors2(btn8);
+        InitializeColors2(btn9);
+        InitializeColors2(btn10);
+
+        InitializeGO(RFID);        
+        InitializeGO(TOUCH);
+        InitializeGO(MOTION);
+        InitializeGO(SOUND);
+
+        InitializeGO(AUDIO);
+        InitializeGO(LED);
+        InitializeGO(TEXT);
+        InitializeGO(MOTOR);
+        InitializeGO(VIDEO);
+        InitializeGO(GRAPHICS);
+    }
+
+    
+
+    public void InitializeColors(Button b)
+    {
+        ColorBlock colors = b.colors;        
+        colors.highlightedColor = new Color32(185, 191, 230, 255);
+        b.colors = colors;
+    }
+    public void InitializeColors2(Button b)
+    {
+        ColorBlock colors = b.colors;        
+        colors.highlightedColor = new Color32(225, 165, 138, 255);
+        b.colors = colors;
+    }
+    public void InitializeGO(GameObject GO)
+    {
+        GO.SetActive(false);
+    }
+    public void HighlightButton(Button b)
+    {        
+        ColorBlock colors = b.colors;
+        colors.normalColor = new Color32(185, 191, 230, 255);        
+        b.colors = colors;        
+
+    }
+
+    public void HighlightButtonLeft(Button b)
+    {
+        ColorBlock colors = b.colors;
+        colors.normalColor = new Color32(225, 165, 138, 255);        
+        b.colors = colors;
+
+    }
+
+    public void DeactivateButton(Button b)
+    {
+        ColorBlock colors = b.colors;        
+        colors.normalColor = Color.white;         
+        b.colors = colors;
+        
+    }
+
+    public void DeactivateButton1(Button b)
+    {
+        ColorBlock colors = b.colors;        
+        colors.normalColor = Color.white;
+        b.colors = colors;
+
+    }
+
+    public void ButtonHandler(int i)
+    {        
+        if (i == 1)
+        {
+            HighlightButton(btn1);
+            DeactivateButton(btn2);
+            DeactivateButton(btn3);
+            DeactivateButton(btn4);
+            RFID.SetActive(true);
+            InitializeGO(MOTION);
+            InitializeGO(TOUCH);
+            InitializeGO(SOUND);
+        }
+        if (i == 2)
+        {
+            HighlightButton(btn2);
+            DeactivateButton(btn1);
+            DeactivateButton(btn3);
+            DeactivateButton(btn4);
+            MOTION.SetActive(true);
+            InitializeGO(RFID);
+            InitializeGO(TOUCH);
+            InitializeGO(SOUND);
+        }
+        if (i == 3)
+        {
+            HighlightButton(btn3);
+            DeactivateButton(btn1);
+            DeactivateButton(btn2);
+            DeactivateButton(btn4);
+            TOUCH.SetActive(true);
+            InitializeGO(MOTION);
+            InitializeGO(RFID);
+            InitializeGO(SOUND);
+        }
+        if (i == 4)
+        {
+            HighlightButton(btn4);
+            DeactivateButton(btn1);
+            DeactivateButton(btn2);
+            DeactivateButton(btn3);
+            SOUND.SetActive(true);
+            InitializeGO(MOTION);
+            InitializeGO(TOUCH);
+            InitializeGO(RFID);
+        }
+
+    }
+        public void ButtonHandler2(int i)
+        {        
+        if (i == 5)
+        {
+            HighlightButtonLeft(btn5);
+            DeactivateButton1(btn6);
+            DeactivateButton1(btn7);
+            DeactivateButton1(btn8);
+            DeactivateButton1(btn9);
+            DeactivateButton1(btn10);
+            AUDIO.SetActive(true);
+            InitializeGO(LED);
+            InitializeGO(TEXT);
+            InitializeGO(MOTOR);
+            InitializeGO(VIDEO);
+            InitializeGO(GRAPHICS);
+        }
+        if (i == 6)
+        {
+            HighlightButtonLeft(btn6);
+            DeactivateButton1(btn5);
+            DeactivateButton1(btn7);
+            DeactivateButton1(btn8);
+            DeactivateButton1(btn9);
+            DeactivateButton1(btn10);
+            LED.SetActive(true);
+            InitializeGO(AUDIO);
+            InitializeGO(TEXT);
+            InitializeGO(MOTOR);
+            InitializeGO(VIDEO);
+            InitializeGO(GRAPHICS);
+        }
+        if (i == 7)
+        {
+            HighlightButtonLeft(btn7);
+            DeactivateButton1(btn6);
+            DeactivateButton1(btn5);
+            DeactivateButton1(btn8);
+            DeactivateButton1(btn9);
+            DeactivateButton1(btn10);
+            TEXT.SetActive(true);
+            InitializeGO(LED);
+            InitializeGO(AUDIO);
+            InitializeGO(MOTOR);
+            InitializeGO(VIDEO);
+            InitializeGO(GRAPHICS);
+        }
+        if (i == 8)
+        {
+            HighlightButtonLeft(btn8);
+            DeactivateButton1(btn6);
+            DeactivateButton1(btn7);
+            DeactivateButton1(btn5);
+            DeactivateButton1(btn9);
+            DeactivateButton1(btn10);
+            MOTOR.SetActive(true);
+            InitializeGO(LED);
+            InitializeGO(TEXT);
+            InitializeGO(AUDIO);
+            InitializeGO(VIDEO);
+            InitializeGO(GRAPHICS);
+        }
+        if (i == 9)
+        {
+            HighlightButtonLeft(btn9);
+            DeactivateButton1(btn6);
+            DeactivateButton1(btn7);
+            DeactivateButton1(btn8);
+            DeactivateButton1(btn5);
+            DeactivateButton1(btn10);
+            VIDEO.SetActive(true);
+            InitializeGO(LED);
+            InitializeGO(TEXT);
+            InitializeGO(MOTOR);
+            InitializeGO(AUDIO);
+            InitializeGO(GRAPHICS);
+        }
+        if (i == 10)
+        {
+            HighlightButtonLeft(btn10);
+            DeactivateButton1(btn6);
+            DeactivateButton1(btn7);
+            DeactivateButton1(btn8);
+            DeactivateButton1(btn9);
+            DeactivateButton1(btn5);
+            GRAPHICS.SetActive(true);
+            InitializeGO(LED);
+            InitializeGO(TEXT);
+            InitializeGO(MOTOR);
+            InitializeGO(VIDEO);
+            InitializeGO(AUDIO);
+        }
+
+    }
+
+
+
+}
--- Assets/Scripts/Logo.cs
+++ Assets/Scripts/Logo.cs
@@ -10,6 +10,35 @@ public class Logo : MonoBehaviour
     public Image img3;
     public Image img4;
 
+    public Image img5;
+    public Image img6;
+    public Image img7;
+    public Image img8;
+    public Image img9;
+    public Image img10;
+
+    public Text txt1;
+    public Text txt2;
+    public Text txt3;
+    public Text txt4;
+
+    public Text txt5;
+    public Text txt6;
+    public Text txt7;
+    public Text txt8;
+    public Text txt9;
+    public Text txt10;
+
+    public Button resetButton;
+    public Button resetButton1;
+    public Text status;
+    public Text status1;
+    public Text READY;
+    public Text NOTREADY;
+    public Text READY1;
+    public Text NOTREADY1;
+
+
 
     void Start()
     {
@@ -17,84 +46,249 @@ public class Logo : MonoBehaviour
         img2.enabled = false;
         img3.enabled = false;
         img4.enabled = false;
+
+        img5.enabled = false;
+        img6.enabled = false;
+        img7.enabled = false;
+        img8.enabled = false;
+        img9.enabled = false;
+        img10.enabled = false;
+
+        txt1.enabled = false;
+        txt2.enabled = false;
+        txt3.enabled = false;
+        txt4.enabled = false;
+        txt5.enabled = false;
+        txt6.enabled = false;
+        txt7.enabled = false;
+        txt8.enabled = false;
+        txt9.enabled = false;
+        txt10.enabled = false;
+
+        resetButton.gameObject.SetActive(false);
+        resetButton1.gameObject.SetActive(false);
+        status.enabled = false;
+        status1.enabled = false;
+        READY.enabled = false;
+        NOTREADY.enabled = false;
+        READY1.enabled = false;
+        NOTREADY1.enabled = false;
+
+
     }
 
-    void Update()
+
+    public void ImageHolder(int i)
     {
-            
-            if (Button.GetLogo() == 1)
-            {
+        if (i == 1)
+        {
+            img1.enabled = true;
+            img2.enabled = false;
+            img3.enabled = false;
+            img4.enabled = false;
 
-                if (img1.enabled == false)
-                {
+            resetButton.gameObject.SetActive(true);
+            status.enabled = true;
+        }
+        if (i == 2)
+        {
+            img1.enabled = false;
+            img2.enabled = true;
+            img3.enabled = false;
+            img4.enabled = false;
 
-                    img1.enabled = true;                    
-                    img2.enabled = false;                    
-                    img3.enabled = false;                    
-                    img4.enabled = false;                    
-                }
+            resetButton.gameObject.SetActive(true);
+            status.enabled = true;
+        }
+        if (i == 3)
+        {
+            img1.enabled = false;
+            img2.enabled = false;
+            img3.enabled = true;
+            img4.enabled = false;
 
-                else
-                {
+            resetButton.gameObject.SetActive(true);
+            status.enabled = true;
+        }
+        if (i == 4)
+        {
+            img1.enabled = false;
+            img2.enabled = false;
+            img3.enabled = false;
+            img4.enabled = true;
 
-                    img1.enabled = false;
-                }
-            }
-            if (Button.GetLogo() == 2)
-            {
+            resetButton.gameObject.SetActive(true);
+            status.enabled = true;
 
-                if (img2.enabled == false)
-                {
-                img1.enabled = false;
-                img2.enabled = true;
-                img3.enabled = false;
-                img4.enabled = false;
-            }
+        }
+        if (i == 5)
+        {
+            img5.enabled = true;
+            img6.enabled = false;
+            img7.enabled = false;
+            img8.enabled = false;
+            img9.enabled = false;
+            img10.enabled = false;
 
-                else
-                {
+            resetButton1.gameObject.SetActive(true);
+            status1.enabled = true;
 
-                    img2.enabled = false;
-                }
-            }
-            if (Button.GetLogo() == 3)
-            {
+        }
+        if (i == 6)
+        {
+            img5.enabled = false;
+            img6.enabled = true;
+            img7.enabled = false;
+            img8.enabled = false;
+            img9.enabled = false;
+            img10.enabled = false;
 
-                if (img3.enabled == false)
-                {
+            resetButton1.gameObject.SetActive(true);
+            status1.enabled = true;
 
-                img1.enabled = false;
-                img2.enabled = false;
-                img3.enabled = true;
-                img4.enabled = false;
-            }
+        }
+        if (i == 7)
+        {
+            img5.enabled = false;
+            img6.enabled = false;
+            img7.enabled = true;
+            img8.enabled = false;
+            img9.enabled = false;
+            img10.enabled = false;
 
-                else
-                {
+            resetButton1.gameObject.SetActive(true);
+            status1.enabled = true;
 
-                    img3.enabled = false;
-                }
-            }
-            if (Button.GetLogo() == 4)
-            {
+        }
+        if (i == 8)
+        {
+            img5.enabled = false;
+            img6.enabled = false;
+            img7.enabled = false;
+            img8.enabled = true;
+            img9.enabled = false;
+            img10.enabled = false;
 
-                if (img4.enabled == false)
-                {
+            resetButton1.gameObject.SetActive(true);
+            status1.enabled = true;
 
-                img1.enabled = false;
-                img2.enabled = false;
-                img3.enabled = false;
-                img4.enabled = true;
-            }
+        }
+        if (i == 9)
+        {
+            img5.enabled = false;
+            img6.enabled = false;
+            img7.enabled = false;
+            img8.enabled = false;
+            img9.enabled = true;
+            img10.enabled = false;
 
-                else
-                {
+            resetButton1.gameObject.SetActive(true);
+            status1.enabled = true;
 
-                    img4.enabled = false;
-                }
-            }
+        }
+        if (i == 10)
+        {
+            img5.enabled = false;
+            img6.enabled = false;
+            img7.enabled = false;
+            img8.enabled = false;
+            img9.enabled = false;
+            img10.enabled = true;
+
+            resetButton1.gameObject.SetActive(true);
+            status1.enabled = true;
+
         }
     }
+
+
+    public void TextHolder(int i)
+    {
+        if (i == 1)
+        {
+            txt1.enabled = true;
+            txt2.enabled = false;
+            txt3.enabled = false;
+            txt4.enabled = false;
+        }
+        if (i == 2)
+        {
+            txt1.enabled = false;
+            txt2.enabled = true;
+            txt3.enabled = false;
+            txt4.enabled = false;
+        }
+        if (i == 3)
+        {
+            txt1.enabled = false;
+            txt2.enabled = false;
+            txt3.enabled = true;
+            txt4.enabled = false;
+        }
+        if (i == 4)
+        {
+            txt1.enabled = false;
+            txt2.enabled = false;
+            txt3.enabled = false;
+            txt4.enabled = true;
+        }
+        if (i == 5)
+        {
+            txt5.enabled = true;
+            txt6.enabled = false;
+            txt7.enabled = false;
+            txt8.enabled = false;
+            txt9.enabled = false;
+            txt10.enabled = false;
+        }
+        if (i == 6)
+        {
+            txt5.enabled = false;
+            txt6.enabled = true;
+            txt7.enabled = false;
+            txt8.enabled = false;
+            txt9.enabled = false;
+            txt10.enabled = false;
+        }
+        if (i == 7)
+        {
+            txt5.enabled = false;
+            txt6.enabled = false;
+            txt7.enabled = true;
+            txt8.enabled = false;
+            txt9.enabled = false;
+            txt10.enabled = false;
+        }
+        if (i == 8)
+        {
+            txt5.enabled = false;
+            txt6.enabled = false;
+            txt7.enabled = false;
+            txt8.enabled = true;
+            txt9.enabled = false;
+            txt10.enabled = false;
+        }
+        if (i == 9)
+        {
+            txt5.enabled = false;
+            txt6.enabled = false;
+            txt7.enabled = false;
+            txt8.enabled = false;
+            txt9.enabled = true;
+            txt10.enabled = false;
+        }
+        if (i == 10)
+        {
+            txt5.enabled = false;
+            txt6.enabled = false;
+            txt7.enabled = false;
+            txt8.enabled = false;
+            txt9.enabled = false;
+            txt10.enabled = true;
+        }
+    }
+}
+        
 
        
 
