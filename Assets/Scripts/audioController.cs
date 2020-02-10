f7c9c668b5cb0e744a2f8effd20521359f65f975
Muhammad Alif Danial <aldmnsri@gmail.com>
2020-02-10 22:41:12 +0800

    Audio Attempt

---
+++ Assets/Scripts/audioController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioController : MonoBehaviour
+{
+    public AudioSource audio;
+    public Text text;
+    string url;
+
+    public void select()
+    {
+        url = audio.clip.name;
+        Debug.Log(url);
+    }
+
+    public void Update()
+    {
+        url = audio.clip.name;
+        text.text = url;
+    }
+}
